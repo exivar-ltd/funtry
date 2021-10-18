227ecfdb92600b8f24a3c1e900e3d7be6ab4eed0
exivar <devops@exivar.com>
2021-10-18 16:28:56 +0800

    updated test

--- src/__tests__/core.test.ts
+++ src/__tests__/core.test.ts
@@ -1,3 +1,31 @@
-test('My Greeter', () => {
-  expect(1+1).toBe(2);
+
+import { asyncTry, syncTry } from '../lib/fnTry';
+
+const promiseTrue = new Promise((resolve, _) => {
+  setTimeout(()=>{
+    resolve({ ok: true })
+  }, 1000)
+})
+const promiseFalse = new Promise((_,reject) => {
+  setTimeout(()=>{
+    reject(Error('error'))
+  }, 1000)
+})
+const functionTrue = (a: number,b: number) => a + b;
+const functionFalse = (a: number,b: number) => Error('error');
+
+
+test('Async Function Test Truthy', async () => {
+  expect(await asyncTry(promiseTrue)).toEqual(expect.arrayContaining([{ok: true}, null]));
+  expect(await asyncTry(promiseFalse)).toEqual(expect.arrayContaining([null, Error('error')]));
+});
+test('Async Function Test Falsy', async () => {
+  expect(await asyncTry(promiseFalse)).toEqual(expect.arrayContaining([null, Error('error')]));
+});
+
+test('Function Test True', () => {
+  expect(syncTry(functionTrue(1,2))).toEqual(expect.arrayContaining([3, null]));
+});
+test('Function Test False', () => {
+  expect(syncTry(functionFalse(1,2))).toEqual(expect.arrayContaining([null, Error('error')]));
 });
