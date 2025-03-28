05cd34c384c968c886930da627a2a689e16055a3
BardZ H <3595255+bardzh@users.noreply.github.com>
2025-03-28 16:14:44 -0400

    adds finally callback

--- src/lib/fnTry.ts
+++ src/lib/fnTry.ts
@@ -1,17 +1,39 @@
-export async function asyncTry<T>(fn: Promise<T> | PromiseLike<T>): Promise<[T, null] | [null, Error]> {
+export async function asyncTry<T>(
+	fn: Promise<T> | PromiseLike<T>,
+	cb?: (() => Promise<any>) | Promise<any>
+): Promise<[T, null] | [null, Error]> {
 	try {
 		const res = await fn;
 		return [res, null];
 	} catch (error) {
 		return [null, error as Error];
+	} finally {
+		if (cb) {
+			if (typeof cb === 'function') {
+				await cb();
+			} else {
+				await cb;
+			}
+		}
 	}
 }
 
-export function syncTry<T>(fn: T): [T, null] | [null, Error] {
+export function syncTry<T>(
+	fn: () => T,
+	cb?: () => void
+): [T, null] | [null, Error]  {
 	try {
 		const res = fn;
 		return [res, null];
 	} catch (error) {
 		return [null, error as Error];
+	} finally {
+		if (cb) {
+			if (typeof cb === 'function') {
+				await cb();
+			} else {
+				await cb;
+			}
+		}
 	}
 }
